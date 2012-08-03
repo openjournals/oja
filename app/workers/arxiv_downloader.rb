23c918db872d4d382448b4c655f30f8bde8969da
Arfon Smith <arfon.smith@gmail.com>
2012-08-03 10:50:15 -0700

    arXiv downloaded now a sidekiq process

---
+++ app/workers/arxiv_downloader.rb
@@ -0,0 +1,35 @@
+class ArxivDownloader
+  include Sidekiq::Worker
+  
+  attr_accessor :arxiv_id
+
+  def perform(arxiv_id)
+    puts "Downloading"
+    download(arxiv_id)
+    initialize_git(arxiv_id)
+  end
+  
+  def download(arxiv_id)
+    `curl "http://arxiv.org/e-print/#{arxiv_id}v1" -o tmp/#{arxiv_id}.tar.gz`
+    `mkdir tmp/#{arxiv_id}`
+    `tar -xf tmp/#{arxiv_id}.tar.gz -C tmp/#{arxiv_id}`
+    `rm tmp/#{arxiv_id}.tar.gz`
+  end
+  
+  def initialize_git(arxiv_id)
+    puts "Creating GitHub repository"
+    repo = GITHUB_CONNECTION.create_repository(arxiv_id)
+    puts "GitHub address: #{repo.ssh_url}"
+    
+    `cd tmp/#{arxiv_id} && git init`
+    `cd tmp/#{arxiv_id} && git add *`
+    
+    puts "Creating initial commit"
+    
+    `cd tmp/#{arxiv_id} && git commit -m 'Adding initial paper'`
+    
+    `cd tmp/#{arxiv_id} && git remote add origin #{repo.ssh_url}`    
+    `ssh-agent bash -c 'ssh-add -D; ssh-add /Users/arfon/Sites/Adler/oja/config/ssh/***REMOVED***_rsa; cd tmp/#{arxiv_id} && git push -u origin master'`
+    puts "Done"
+  end
+end
