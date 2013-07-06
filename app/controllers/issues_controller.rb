9896f0f01c281c0a338b48d634c74eea22c75b26
Arfon Smith <arfon.smith@gmail.com>
2013-07-06 14:57:54 -0500

    Issue creation endpoint

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -9,6 +9,20 @@ class IssuesController < ApplicationController
     respond_with issues
   end
 
+  # expecting title, text
+  def create
+    title = params[:issue][:title]
+    text = params[:issue][:text]
+    
+    begin
+      issue = @paper.add_issue(title, text)
+      respond_with issue, :status => :created
+    rescue => e
+      puts "Oh noes!"
+      respond_with {}, :status => :internal_server_error
+    end
+  end
+
   private
 
   def find_paper
