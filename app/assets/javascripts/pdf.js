0ff874669227f361779399a8041121b97b19b25f
Arfon Smith <arfon.smith@gmail.com>
2013-08-11 21:07:54 -0500

    Fixing Uglifier?

--- app/assets/javascripts/pdf.js
+++ app/assets/javascripts/pdf.js
@@ -20731,6 +20731,9 @@ var Font = (function FontClosure() {
     var winAscent = override.yMax || typoAscent;
     var winDescent = -override.yMin || -typoDescent;
 
+    // see this https://github.com/mozilla/pdf.js/issues/2209
+    var x31 = '\x31'; // to prevent converting by uglifier
+    
     return '\x00\x03' + // version
            '\x02\x24' + // xAvgCharWidth
            '\x01\xF4' + // usWeightClass
@@ -20744,7 +20747,7 @@ var Font = (function FontClosure() {
            '\x02\xBB' + // ySuperScriptYSize
            '\x00\x00' + // ySuperScriptXOffset
            '\x01\xDF' + // ySuperScriptYOffset
-           '\x00\x31' + // yStrikeOutSize
+           '\x00'+x31 + // yStrikeOutSize
            '\x01\x02' + // yStrikeOutPosition
            '\x00\x00' + // sFamilyClass
            '\x00\x00\x06' +
